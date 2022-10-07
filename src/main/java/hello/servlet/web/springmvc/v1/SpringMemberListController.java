c64eedfc3ddfbd9948189513aa0c124e8ec7c104
hojae29 <gkqthd6386@naver.com>
2022-10-07 15:21:16 +0900

    Spring MVC - 시작하기

---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListController.java
@@ -0,0 +1,25 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListController {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process(){
+        List<Member> list = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", list);
+
+        return mv;
+    }
+}
